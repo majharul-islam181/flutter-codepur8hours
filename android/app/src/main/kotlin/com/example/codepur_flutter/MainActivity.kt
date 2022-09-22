6be1503c8b3fdb7a80bb6c3bd0786341333382e1
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-09-22 22:16:07 +0600

    fluttercodepur8

---
+++ android/app/src/main/kotlin/com/example/codepur_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codepur_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyhomePage extends StatelessWidget {
+  const MyhomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('this is sample field!'),
+        )
+      ),
+
+    );
+  }
+}
